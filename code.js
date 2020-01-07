4f5b4a11b0069fbbce296202b872250b8939face
vroenVen <42852953+vroenVen@users.noreply.github.com>
2020-01-06 19:31:38 -0500

    Stop double tap zoom

--- code.js
+++ code.js
@@ -85,4 +85,12 @@ function embark(){
 		textupdate();
 	}
 }
+var doubleTouchStartTimestamp = 0;
+document.addEventListener("touchstart", function(event){
+    var now = +(new Date());
+    if (doubleTouchStartTimestamp + 500 > now){
+        event.preventDefault();
+    };
+    doubleTouchStartTimestamp = now;
+});
 textupdate();
